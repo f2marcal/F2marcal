3b84e1473dc2a3f85d0987369c8826d0c76a1afe
f2marcal <94324648+f2marcal@users.noreply.github.com>
2021-12-03 01:57:17 -0300

    Add files via upload

--- financeplus.py
+++ financeplus.py
@@ -4087,7 +4087,7 @@ if col4 == "INDICADORES NÍVEL II ":
             return rsi
 
 
-        sigla['RSI'] = computeRSI(btc_df[float('close')], 14)
+        sigla['RSI'] = computeRSI(float(btc_df['close']), 14)
 
 
         def stochastic(data, k_window, d_window, window):
