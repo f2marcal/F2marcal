5cdd433530a832dadf7bb291074567f5ec4e3710
f2marcal <94324648+f2marcal@users.noreply.github.com>
2021-12-04 14:37:32 -0300

    Add files via upload

--- financeplus.py
+++ financeplus.py
@@ -4062,50 +4062,6 @@ if col4 == "INDICADORES NÍVEL II ":
         df = btc_df
 
 
-        # calculating RSI (gives the same values as TradingView)
-        # https://stackoverflow.com/questions/20526414/relative-strength-index-in-python-pandas
-        def RSI(series, period=14):
-            delta = series.diff().dropna()
-            ups = delta * 0
-            downs = ups.copy()
-            ups[delta > 0] = delta[delta > 0]
-            downs[delta < 0] = -delta[delta < 0]
-            ups[ups.index[period - 1]] = np.mean(ups[:period])  # first value is sum of avg gains
-            ups = ups.drop(ups.index[:(period - 1)])
-            downs[downs.index[period - 1]] = np.mean(downs[:period])  # first value is sum of avg losses
-            downs = downs.drop(downs.index[:(period - 1)])
-            rs = ups.ewm(com=period - 1, min_periods=0, adjust=False, ignore_na=False).mean() / \
-                 downs.ewm(com=period - 1, min_periods=0, adjust=False, ignore_na=False).mean()
-            return 100 - 100 / (1 + rs)
-
-
-        df['RSI'] = RSI(df, 14)
-
-
-        # calculating Stoch RSI (gives the same values as TradingView)
-        # https://www.tradingview.com/wiki/Stochastic_RSI_(STOCH_RSI)
-        def StochRSI(series, period=14, smoothK=3, smoothD=3):
-            # Calculate RSI
-            delta = series.diff().dropna()
-            ups = delta * 0
-            downs = ups.copy()
-            ups[delta > 0] = delta[delta > 0]
-            downs[delta < 0] = -delta[delta < 0]
-            ups[ups.index[period - 1]] = np.mean(ups[:period])  # first value is sum of avg gains
-            ups = ups.drop(ups.index[:(period - 1)])
-            downs[downs.index[period - 1]] = np.mean(downs[:period])  # first value is sum of avg losses
-            downs = downs.drop(downs.index[:(period - 1)])
-            rs = ups.ewm(com=period - 1, min_periods=0, adjust=False, ignore_na=False).mean() / \
-                 downs.ewm(com=period - 1, min_periods=0, adjust=False, ignore_na=False).mean()
-            rsi = 100 - 100 / (1 + rs)
-
-            # Calculate StochRSI
-            stochrsi = (rsi - rsi.rolling(period).min()) / (rsi.rolling(period).max() - rsi.rolling(period).min())
-            stochrsi_K = stochrsi.rolling(smoothK).mean()
-            stochrsi_D = stochrsi_K.rolling(smoothD).mean()
-
-            return stochrsi, stochrsi_K, stochrsi_D
-
 
         # calculating Stoch RSI
         #  -- Same as the above function but uses EMA, not SMA
